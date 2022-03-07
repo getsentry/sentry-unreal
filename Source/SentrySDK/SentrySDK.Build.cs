88400bdae0e9c3d5238a197480e61b608c837294
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-03-07 17:40:16 +0200

    Add native iOS framework

--- Source/SentrySDK/SentrySDK.Build.cs
+++ Source/SentrySDK/SentrySDK.Build.cs
@@ -1,5 +1,7 @@
 // Copyright (c) 2022 Sentry. All Rights Reserved.
 
+using System.IO;
+using Tools.DotNETCommon;
 using UnrealBuildTool;
 
 public class SentrySDK : ModuleRules
@@ -14,14 +16,14 @@ public class SentrySDK : ModuleRules
 				// ... add public include paths required here ...
 			}
 		);
-		
+
 		PrivateIncludePaths.AddRange(
 			new string[]
 			{
 				// ... add other private include paths required here ...
 			}
 		);
-		
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -29,7 +31,7 @@ public class SentrySDK : ModuleRules
 				// ... add other public dependencies that you statically link with here ...
 			}
 		);
-		
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
@@ -37,15 +39,27 @@ public class SentrySDK : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"Settings"
 				// ... add private dependencies that you statically link with here ...	
 			}
 		);
-		
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
 				// ... add any modules that your module loads dynamically here ...
 			}
 		);
+
+		// Additional routine for iOS
+		if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			PublicAdditionalFrameworks.Add(new Framework("Sentry", "../ThirdParty/IOS/Sentry.embeddedframework.zip"));
+			
+			PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
+			string PluginPath = Utils.MakePathRelativeTo(ModuleDirectory, Target.RelativeEnginePath);
+
+			AdditionalPropertiesForReceipt.Add("IOSPlugin", Path.Combine(PluginPath, "SentrySDK_IOS_UPL.xml"));
+		}
 	}
 }
