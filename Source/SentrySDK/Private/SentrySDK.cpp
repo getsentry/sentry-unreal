15ca4682fdadf1bb3f0645fcfe4fd0122e5cb58f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-03-07 13:52:18 +0200

    Register plugin settings editor panel

--- Source/SentrySDK/Private/SentrySDK.cpp
+++ Source/SentrySDK/Private/SentrySDK.cpp
@@ -2,6 +2,10 @@
 
 #include "SentrySDK.h"
 #include "SentryDefines.h"
+#include "SentrySettings.h"
+
+#include "Modules/ModuleManager.h"
+#include "ISettingsModule.h"
 
 #define LOCTEXT_NAMESPACE "FSentrySDKModule"
 
@@ -10,16 +14,56 @@ const FName FSentrySDKModule::ModuleName = "SentrySDK";
 void FSentrySDKModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	SentrySettings = NewObject<USentrySettings>(GetTransientPackage(), "SentrySettings", RF_Standalone);
+	SentrySettings->AddToRoot();
+
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		SettingsModule->RegisterSettings("Project", "Plugins", "SentrySdk",
+			LOCTEXT("RuntimeSettingsName", "Sentry SDK"),
+			LOCTEXT("RuntimeSettingsDescription", "Configure Sentry SDK"),
+			SentrySettings);
+	}
 }
 
 void FSentrySDKModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+
+	if (ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+	{
+		SettingsModule->UnregisterSettings("Project", "Plugins", "SentrySdk");
+	}
+
+	if (!GExitPurge)
+	{
+		// If we're in exit purge, this object has already been destroyed
+		SentrySettings->RemoveFromRoot();
+	}
+	else
+	{
+		SentrySettings = nullptr;
+	}
+}
+
+FSentrySDKModule& FSentrySDKModule::Get()
+{
+	return FModuleManager::LoadModuleChecked<FSentrySDKModule>(ModuleName);
+}
+
+bool FSentrySDKModule::IsAvailable()
+{
+	return FModuleManager::Get().IsModuleLoaded(ModuleName);
+}
+
+USentrySettings* FSentrySDKModule::GetSettings() const
+{
+	return SentrySettings;
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FSentrySDKModule, SentrySDK)
 
 DEFINE_LOG_CATEGORY(LogSentrySdk);
--- Source/SentrySDK/Public/SentrySDK.h
+++ Source/SentrySDK/Public/SentrySDK.h
@@ -3,7 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Modules/ModuleManager.h"
+
+class USentrySettings;
 
 class FSentrySDKModule : public IModuleInterface
 {
@@ -13,25 +14,25 @@ public:
 	virtual void ShutdownModule() override;
 
 	/**
-	 * Singleton-like access to this module's interface.  This is just for convenience!
-	 * Beware of calling this during the shutdown phase, though.  Your module might be already uploaded.
+	 * Singleton-like access to this module's interface. This is just for convenience!
+	 * Beware of calling this during the shutdown phase, though. Your module might be already unloaded.
 	 *
-	 * @return Returns singleton instance, loading the module on demand if needed
+	 * @return Returns singleton instance, loading the module on demand if needed.
 	 */
-	static inline FSentrySDKModule& Get()
-	{
-		return FModuleManager::LoadModuleChecked<FSentrySDKModule>(ModuleName);
-	}
+	static inline FSentrySDKModule& Get();
 
 	/**
-	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	 * Check if this module is loaded and ready. It is only valid to call Get() if IsAvailable() returns true.
 	 *
 	 * @return True if the module is loaded and ready to use.
 	 */
-	static inline bool IsAvailable()
-	{
-		return FModuleManager::Get().IsModuleLoaded(ModuleName);
-	}
+	static inline bool IsAvailable();
+
+	/** Getter for internal settings object to support runtime configuration changes. */
+	USentrySettings* GetSettings() const;
 
 	static const FName ModuleName;
+
+private:
+	USentrySettings* SentrySettings = nullptr;
 };
