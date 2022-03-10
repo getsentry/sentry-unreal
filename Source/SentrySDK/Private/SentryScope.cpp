c212a39467df3d3ac23be6b84319e6ba3ff8f804
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-03-10 11:09:30 +0200

    Add stubs for some scope class methods

--- Source/SentrySDK/Private/SentryScope.cpp
+++ Source/SentrySDK/Private/SentryScope.cpp
@@ -7,10 +7,59 @@
 #include "IOS/SentryConvertorsIOS.h"
 #endif
 
-void USentryScope::SetLevel(ESentryLevel level)
+void USentryScope::SetTagValue(const FString& Key, const FString& Value)
 {
 #if PLATFORM_IOS
-	[_scopeIOS setLevel:SentryConvertorsIOS::SentryLevelToNative(level)];
+	// TODO
+#endif
+}
+
+void USentryScope::RemoveTag(const FString& Key)
+{
+#if PLATFORM_IOS
+	// TODO
+#endif
+}
+
+void USentryScope::SetTags(const TMap<FString, FString>& Tags)
+{
+#if PLATFORM_IOS
+	// TODO
+#endif
+}
+
+void USentryScope::SetDist(const FString& Dist)
+{
+#if PLATFORM_IOS
+	// TODO
+#endif
+}
+
+void USentryScope::SetEnvironment(const FString& Environment)
+{
+#if PLATFORM_IOS
+	// TODO
+#endif
+}
+
+void USentryScope::SetFingerprint(const TArray<FString>& Fingerprint)
+{
+#if PLATFORM_IOS
+	// TODO
+#endif
+}
+
+void USentryScope::SetLevel(ESentryLevel Level)
+{
+#if PLATFORM_IOS
+	[_scopeIOS setLevel:SentryConvertorsIOS::SentryLevelToNative(Level)];
+#endif
+}
+
+void USentryScope::Clear()
+{
+#if PLATFORM_IOS
+	// TODO
 #endif
 }
 
--- Source/SentrySDK/Public/SentryScope.h
+++ Source/SentrySDK/Public/SentryScope.h
@@ -16,8 +16,37 @@ class SENTRYSDK_API USentryScope : public UObject
 	GENERATED_BODY()
 
 public:
+	/** Sets a global tag. Tags are searchable key/value string pairs attached to every event. */
 	UFUNCTION(BlueprintCallable, Category = "Sentry")
-	void SetLevel(ESentryLevel level);
+	void SetTagValue(const FString& Key, const FString& Value);
+
+	/** Removes the tag for the specified key. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void RemoveTag(const FString& Key);
+
+	/** Sets global tags. Tags are searchable key/value string pairs attached to every event. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void SetTags(const TMap<FString, FString>& Tags);
+
+	/** Sets dist in the scope. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void SetDist(const FString& Dist);
+
+	/** Sets environment in the scope. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void SetEnvironment(const FString& Environment);
+
+	/** Sets fingerprint in the scope. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void SetFingerprint(const TArray<FString>& Fingerprint);
+
+	/** Sets the level in the scope. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void SetLevel(ESentryLevel Level);
+
+	/** Clears the current scope. */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	void Clear();
 
 #if PLATFORM_IOS
 	void InitWithNativeObjectIOS(SentryScope* scope);
