a81c8bf0303cc16ba93bf5cff7775550c81bf0dc
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-04-11 19:01:50 +0300

    Add message and event capturing automation tests for Sentry subsystem

---
+++ Source/SentrySDK/Private/Tests/SentrySubsystem.spec.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2022 Sentry. All Rights Reserved.
+
+#include "SentryEvent.h"
+#include "SentryModule.h"
+#include "Desktop/SentrySubsystemDesktop.h"
+
+#include "Misc/AutomationTest.h"
+
+#if WITH_AUTOMATION_TESTS
+
+BEGIN_DEFINE_SPEC(SentrySubsystemSpec, "Sentry.SentrySubsystem", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+	TSharedPtr<ISentrySubsystem> SentrySubsystemDesktopImpl;
+END_DEFINE_SPEC(SentrySubsystemSpec)
+
+void SentrySubsystemSpec::Define()
+{
+	BeforeEach([this]()
+	{
+		SentrySubsystemDesktopImpl = MakeShareable(new SentrySubsystemDesktop());
+
+		const USentrySettings* Settings = FSentryModule::Get().GetSettings();
+		SentrySubsystemDesktopImpl->InitWithSettings(Settings);
+	});
+
+	Describe("Capture Message", [this]()
+	{
+		It("should return a non-null Event ID if message captured", [this]()
+		{
+			const USentryId* eventId = SentrySubsystemDesktopImpl->CaptureMessage(FString(TEXT("Automation: Sentry test message")), ESentryLevel::Debug);
+			TestNotNull("Event ID is non-null", eventId);
+		});
+
+		It("should always return null if scoped version used", [this]()
+		{
+			const FConfigureScopeDelegate testDelegate;
+			const USentryId* eventId = SentrySubsystemDesktopImpl->CaptureMessage(FString(TEXT("Automation: Sentry test message with scope")), testDelegate, ESentryLevel::Debug);
+			TestNull("Event ID is null", eventId);
+		});
+	});
+
+	Describe("Capture Event", [this]()
+	{
+		It("should return a non-null Event ID if event captured", [this]()
+		{
+			USentryEvent* testEvent = NewObject<USentryEvent>();
+			testEvent->SetMessage(TEXT("Automation: Sentry test event message"));
+
+			const USentryId* eventId = SentrySubsystemDesktopImpl->CaptureEvent(testEvent);
+			TestNotNull("Event ID is non-null", eventId);
+		});
+
+		It("should always return null if scoped version used", [this]()
+		{
+			USentryEvent* testEvent = NewObject<USentryEvent>();
+			testEvent->SetMessage(TEXT("Automation: Sentry test event message"));
+
+			const FConfigureScopeDelegate testDelegate;
+
+			const USentryId* eventId = SentrySubsystemDesktopImpl->CaptureEventWithScope(testEvent, testDelegate);
+			TestNull("Event ID is null", eventId);
+		});
+	});
+}
+
+#endif
