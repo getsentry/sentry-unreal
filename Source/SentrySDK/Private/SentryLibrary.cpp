3a630399e17806e0d94af76d5117cb1739d22c38
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-04-21 12:08:11 +0300

    Add convenience function for creating some Sentry entities

--- Source/SentrySDK/Private/SentryLibrary.cpp
+++ Source/SentrySDK/Private/SentryLibrary.cpp
@@ -1,6 +1,57 @@
 // Copyright (c) 2022 Sentry. All Rights Reserved.
 
 #include "SentryLibrary.h"
+#include "SentryEvent.h"
+#include "SentryUser.h"
+#include "SentryUserFeedback.h"
+#include "SentryId.h"
+
+USentryEvent* USentryLibrary::CreateSentryEvent(const FString& Message, ESentryLevel Level)
+{
+	USentryEvent* Event = NewObject<USentryEvent>();
+
+	if(!Message.IsEmpty())
+		Event->SetMessage(Message);
+
+	Event->SetLevel(Level);
+
+	return Event;
+}
+
+USentryUser* USentryLibrary::CreateSentryUser(const FString& Email, const FString& Id, const FString& Username, const FString& IpAddress, const TMap<FString, FString>& Data)
+{
+	USentryUser* User = NewObject<USentryUser>();
+
+	if(!Email.IsEmpty())
+		User->SetEmail(Email);
+	if(!Id.IsEmpty())
+		User->SetId(Id);
+	if(!Username.IsEmpty())
+		User->SetUsername(Username);
+	if(!IpAddress.IsEmpty())
+		User->SetIpAddress(IpAddress);
+
+	if(Data.Num() > 0)
+		User->SetData(Data);
+
+	return User;
+}
+
+USentryUserFeedback* USentryLibrary::CreateSentryUserFeedback(USentryId* EventId, const FString& Name, const FString& Email, const FString& Comments)
+{
+	USentryUserFeedback* UserFeedback = NewObject<USentryUserFeedback>();
+
+	UserFeedback->Initialize(EventId);
+
+	if(!Name.IsEmpty())
+		UserFeedback->SetName(Name);
+	if(!Email.IsEmpty())
+		UserFeedback->SetEmail(Email);
+	if(!Comments.IsEmpty())
+		UserFeedback->SetComment(Comments);
+
+	return UserFeedback;
+}
 
 void USentryLibrary::Crash()
 {
--- Source/SentrySDK/Public/SentryLibrary.h
+++ Source/SentrySDK/Public/SentryLibrary.h
@@ -4,7 +4,14 @@
 
 #include "Kismet/BlueprintFunctionLibrary.h"
 
+#include "SentryDataTypes.h"
+
 #include "SentryLibrary.generated.h"
+
+class USentryEvent;
+class USentryUser;
+class USentryUserFeedback;
+class USentryId;
 
 UCLASS()
 class SENTRYSDK_API USentryLibrary : public UBlueprintFunctionLibrary
@@ -12,6 +19,39 @@ class SENTRYSDK_API USentryLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
+	/**
+	 * Creates the event.
+	 *
+	 * @param Message Message to sent.
+	 * @param Level Level of the event.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	static USentryEvent* CreateSentryEvent(const FString& Message, ESentryLevel Level);
+
+	/**
+	 * Creates user.
+	 *
+	 * @param Email Email address of the user.
+	 * @param Id Id of the user.
+	 * @param Username Username of the user.
+	 * @param IpAddress IP address of the user.
+	 * @param Data Additional arbitrary fields related to the user.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sentry", meta = (AutoCreateRefTerm = "Data"))
+	static USentryUser* CreateSentryUser(const FString& Email, const FString& Id, const FString& Username, const FString& IpAddress,
+		const TMap<FString, FString>& Data);
+
+	/**
+	 * Creates user feedback for the event.
+	 *
+	 * @param EventId Id of the event to which user feedback is associated.
+	 * @param Name Name of the user.
+	 * @param Email Email of the user.
+	 * @param Comments Comments of the user about what happened.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sentry")
+	static USentryUserFeedback* CreateSentryUserFeedback(USentryId* EventId, const FString& Name, const FString& Email, const FString& Comments);
+
 	/** Crashes the application. */
 	UFUNCTION(BlueprintCallable, Category = "Sentry")
 	static void Crash();
