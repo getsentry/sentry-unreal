7566968e94a9d6f553eb4957d548e80e885ea7d0
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-08-17 13:07:47 +0200

    remove prebuilt artifacts from the repo

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 export sentryNativeRoot=$1
 export sentryArtifactsDestination=$2
 
-rm -rf "${sentryArtifactsDestination}/bin/"*
-rm -rf "${sentryArtifactsDestination}/include/"*
+rm -rf "${sentryArtifactsDestination}/"*
 
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config Release --parallel
+
+mkdir "${sentryArtifactsDestination}/bin"
+mkdir "${sentryArtifactsDestination}/include"
 
 cp "${sentryNativeRoot}/build/libsentry.dylib" "${sentryArtifactsDestination}/bin/libsentry.dylib"
 cp -R "${sentryNativeRoot}/build/libsentry.dylib.dSYM" "${sentryArtifactsDestination}/bin/libsentry.dylib.dSYM"
--- scripts/build-win64.sh
+++ scripts/build-win64.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 export sentryNativeRoot=$1
 export sentryArtifactsDestination=$2
 
-rm -rf "${sentryArtifactsDestination}/bin/"*
-rm -rf "${sentryArtifactsDestination}/lib/"*
-rm -rf "${sentryArtifactsDestination}/include/"*
+rm -rf "${sentryArtifactsDestination}/"*
 
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config Release --parallel
+
+mkdir "${sentryArtifactsDestination}/bin"
+mkdir "${sentryArtifactsDestination}/include"
+mkdir "${sentryArtifactsDestination}/lib"
 
 cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.lib ${sentryArtifactsDestination}/lib/sentry.lib
 cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.dll ${sentryArtifactsDestination}/bin/sentry.dll
