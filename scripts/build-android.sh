da1d0cd581393386a973a94538bd4cb7cb927028
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-08-17 11:11:18 +0200

    add SDK dependencies build to CI

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -7,7 +7,9 @@ export sentryArtifactsDestination=$2
 rm -rf "${sentryArtifactsDestination}/"*
 
 pushd ${sentryJavaRoot}
-"./gradlew" -PsentryAndroidSdkName=sentry.native.android.unreal :sentry-android-core:assembleRelease :sentry-android-ndk:assembleRelease :sentry:jar --no-daemon --stacktrace --warning-mode none
+./gradlew -PsentryAndroidSdkName=sentry.native.android.unreal \
+    :sentry-android-core:assembleRelease :sentry-android-ndk:assembleRelease :sentry:jar \
+    --no-daemon --stacktrace --warning-mode none
 popd
 
 cp "${sentryJavaRoot}/sentry-android-ndk/build/outputs/aar/sentry-android-ndk-release.aar" "${sentryArtifactsDestination}/sentry-android-ndk-release.aar"
---
+++ scripts/build-win64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+export sentryNativeRoot=$1
+export sentryArtifactsDestination=$2
+
+rm -rf "${sentryArtifactsDestination}/bin/"*
+rm -rf "${sentryArtifactsDestination}/lib/"*
+rm -rf "${sentryArtifactsDestination}/include/"*
+
+cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal
+cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
+cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config Release --parallel
+
+cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.lib ${sentryArtifactsDestination}/lib/sentry.lib
+cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.dll ${sentryArtifactsDestination}/bin/sentry.dll
+cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.pdb ${sentryArtifactsDestination}/bin/sentry.pdb
+cp ${sentryNativeRoot}/build/crashpad_build/handler/Release/crashpad_handler.exe ${sentryArtifactsDestination}/bin/crashpad_handler.exe
+cp ${sentryNativeRoot}/include/sentry.h ${sentryArtifactsDestination}/include/sentry.h
