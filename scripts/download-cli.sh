9e9b91f314f6cddbddb57919a45f6eb16f0d9698
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-08-18 16:00:01 +0300

    Fix download script

--- scripts/download-cli.sh
+++ scripts/download-cli.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+function getProperty {
+    cat "$(dirname $0)/../modules/sentry-cli.properties" | grep $1 | cut -d'=' -f2 | tr -d '"'
+}
+
+baseUrl="$(getProperty 'repo')/releases/download/$(getProperty 'version')/sentry-cli-"
+
 mkdir -p "$(dirname $0)/../plugin-dev/Source/ThirdParty"
 cd "$(dirname $0)/../plugin-dev/Source/ThirdParty"
 mkdir -p "CLI"
 cd "CLI"
-
-function getProperty {
-    cat "../modules/sentry-cli.properties" | grep $1 | cut -d'=' -f2 | tr -d '"'
-}
-
-baseUrl="$(getProperty 'repo')/releases/download/$(getProperty 'version')/sentry-cli-"
 
 declare -a platforms=("Darwin-universal" "Linux-x86_64" "Windows-x86_64")
 for platform in "${platforms[@]}"; do
