f0c1c395bcaaffe00bd3bb98df01c01e533c40af
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-08-19 13:24:16 +0200

    ci: change java dependency definition (made possible by #68)

--- scripts/bump-version-android.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-sed -i 's/io.sentry,sentry,.\..\../io.sentry,sentry,'$1'/g' Source/Sentry/Sentry_Android_UPL.xml
-sed -i 's/io.sentry,sentry-android-core,.\..\../io.sentry,sentry-android-core,'$1'/g' Source/Sentry/Sentry_Android_UPL.xml
-sed -i 's/io.sentry,sentry-android-ndk,.\..\../io.sentry,sentry-android-ndk,'$1'/g' Source/Sentry/Sentry_Android_UPL.xml
