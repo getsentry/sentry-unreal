4fa854f974529807ecf2ce5910a33fa1402e9e95
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-08-21 12:03:56 +0300

    Fix issue with reading sentry.properties file path from settings on Mac

--- plugin-dev/Scripts/upload-debug-symbols.sh
+++ plugin-dev/Scripts/upload-debug-symbols.sh
@@ -38,7 +38,7 @@ if [ $UPLOAD_SYMBOLS != "True" ]; then
     exit
 fi
 
-PROP_FILE_PATH=$(awk -F \" '/PropertiesFilePath/ {print $2}' ${CONFIG_PATH}/DefaultEngine.ini)
+PROP_FILE_PATH=$(awk -F "=" '/PropertiesFilePath/ {print $2}' ${CONFIG_PATH}/DefaultEngine.ini)
 
 export SENTRY_PROPERTIES="$projectPath/$PROP_FILE_PATH"
 
