aa0a8a433d88cfebd025a7bee252834dad73ccb2
Eddie Stubbington <eddiestubbington@icloud.com>
2022-08-26 16:43:07 +0100

    Update SentryLibrary.cpp to suppress warnings (#86)

--- plugin-dev/Source/Sentry/Private/SentryLibrary.cpp
+++ plugin-dev/Source/Sentry/Private/SentryLibrary.cpp
@@ -89,7 +89,10 @@ USentryAttachment* USentryLibrary::CreateSentryAttachmentWithPath(const FString&
 
 void USentryLibrary::Crash()
 {
+	// Supressing warnings for when using warnings as errors on the target. 
+	#pragma warning(suppress: 6011)	
 	char *ptr = 0;
+	#pragma warning(suppress: 6011)
 	*ptr += 1;
 }
 
