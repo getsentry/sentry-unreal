218b7840270b5417ec43bca03856c906f24dc932
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-09-26 14:56:13 +0300

    Fix plugin packaging issue

--- plugin-dev/Source/Sentry/Private/IOS/SentrySubsystemIOS.cpp
+++ plugin-dev/Source/Sentry/Private/IOS/SentrySubsystemIOS.cpp
@@ -21,14 +21,12 @@
 
 void SentrySubsystemIOS::InitWithSettings(const USentrySettings* settings)
 {
-	NSMutableDictionary* dictOptions = [[NSMutableDictionary alloc] init];
-	dictOptions[@"sdk"] = @{ @"name" : @"sentry.cocoa.unreal" };
+	[PrivateSentrySDKOnly setSdkName:@"sentry.cocoa.unreal"];
 
-	SentryOptions* options = [[SentryOptions alloc] initWithDict:dictOptions didFailWithError:nil];
-	options.dsn = settings->DsnUrl.GetNSString();
-  options.releaseName = settings->Release.GetNSString();
-
-	[SentrySDK startWithOptionsObject:options];
+	[SentrySDK startWithConfigureOptions:^(SentryOptions *options) {
+		options.dsn = settings->DsnUrl.GetNSString();
+		options.releaseName = settings->Release.GetNSString();
+	}];
 }
 
 void SentrySubsystemIOS::Close()
