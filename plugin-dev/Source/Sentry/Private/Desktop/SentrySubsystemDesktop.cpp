bbff4141d7e450ddfb9c544557e823f7d9e9125a
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-10-03 11:13:09 +0300

    Test #8

--- plugin-dev/Source/Sentry/Private/Desktop/SentrySubsystemDesktop.cpp
+++ plugin-dev/Source/Sentry/Private/Desktop/SentrySubsystemDesktop.cpp
@@ -47,13 +47,15 @@ void SentrySubsystemDesktop::InitWithSettings(const USentrySettings* settings)
 		return;
 	}
 
-	const FString DatabasePath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForWrite(*FPaths::Combine(FPaths::ProjectSavedDir(), ".sentry-native"));
+	const FString DatabasePath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForWrite(*FPaths::Combine(FPaths::ProjectSavedDir(), TEXT(".sentry-native")));
+
+	UE_LOG(LogSentrySdk, Log, TEXT("IVAN %s"), *FString(DatabasePath));
 
 	sentry_options_t* options = sentry_options_new();
 	sentry_options_set_dsn(options, TCHAR_TO_ANSI(*settings->DsnUrl));
 	sentry_options_set_release(options, TCHAR_TO_ANSI(*settings->Release));
 	sentry_options_set_handler_path(options, TCHAR_TO_ANSI(*HandlerPath));
-	// sentry_options_set_database_path(options, TCHAR_TO_ANSI(*DatabasePath));
+	sentry_options_set_database_path(options, TCHAR_TO_ANSI(*DatabasePath));
 	sentry_options_set_logger(options, PrintVerboseLog, nullptr);
 	sentry_options_set_debug(options, settings->EnableVerboseLogging);
 
