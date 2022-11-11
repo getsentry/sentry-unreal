165b59b0f9d23a69cecf04f9bc907b326e3b0a69
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-11-11 14:37:49 +0200

    Add editor for automatic CRC configuration

--- plugin-dev/Source/SentryEditor/Private/SentryEditorModule.cpp
+++ plugin-dev/Source/SentryEditor/Private/SentryEditorModule.cpp
@@ -9,6 +9,8 @@
 
 #define LOCTEXT_NAMESPACE "FSentryEditorModule"
 
+const FName FSentryEditorModule::ModuleName = "SentryEditor";
+
 void FSentryEditorModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
@@ -22,6 +24,11 @@ void FSentryEditorModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+}
+
+FSentryEditorModule& FSentryEditorModule::Get()
+{
+	return FModuleManager::LoadModuleChecked<FSentryEditorModule>(ModuleName);
 }
 
 #undef LOCTEXT_NAMESPACE
