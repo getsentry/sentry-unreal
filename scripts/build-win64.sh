b44e6c83631e79d571c996a7f782b7a07e824ac8
Ivan Tustanivskyi <tustanivsky@gmail.com>
2022-12-05 11:48:25 +0200

    Test lib config

--- scripts/build-win64.sh
+++ scripts/build-win64.sh
@@ -6,16 +6,15 @@ export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
 
-cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal
-cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
-cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config Release --parallel
+cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal SENTRY_BUILD_SHARED_LIBS=OFF -D SENTRY_BUILD_SHARED_LIBS=OFF
+cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo
+cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config Release
+cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install" --config RelWithDebInfo
 
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
 mkdir "${sentryArtifactsDestination}/lib"
 
-cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.lib ${sentryArtifactsDestination}/lib/sentry.lib
-cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.dll ${sentryArtifactsDestination}/bin/sentry.dll
-cp ${sentryNativeRoot}/build/RelWithDebInfo/sentry.pdb ${sentryArtifactsDestination}/bin/sentry.pdb
+cp -r ${sentryNativeRoot}/install/lib ${sentryArtifactsDestination}/lib
 cp ${sentryNativeRoot}/build/crashpad_build/handler/Release/crashpad_handler.exe ${sentryArtifactsDestination}/bin/crashpad_handler.exe
 cp ${sentryNativeRoot}/include/sentry.h ${sentryArtifactsDestination}/include/sentry.h
