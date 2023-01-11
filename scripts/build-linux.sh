1331997bc02a85168fd2272dfd77142484792808
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2023-01-11 16:13:00 +0100

    chore: build sentry-native for older GLIBC

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -4,10 +4,15 @@ set -euo pipefail
 export sentryNativeRoot=$1
 export sentryArtifactsDestination=$2
 
-rm -rf "${sentryArtifactsDestination}/"*
+rm -rf "${sentryArtifactsDestination}"
+rm -rf "${sentryNativeRoot}/build"
 
-cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo
-cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
+# Build for an older GLIBC so that it can run with the old GLIBC included in EpicGames' Unreal docker images.
+# See discussion in https://github.com/getsentry/sentry-unreal/pull/173
+docker run --rm dockcross/manylinux_2_28-x64 >./dockcross
+chmod +x ./dockcross
+./dockcross bash -c "cmake -S '${sentryNativeRoot}' -B '${sentryNativeRoot}/build' -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo"
+./dockcross bash -c "cmake --build '${sentryNativeRoot}/build' --target sentry --parallel"
 
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
