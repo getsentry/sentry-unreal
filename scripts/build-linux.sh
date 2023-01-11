fce29fd70a61ad2468646364fe5599f96015c8dc
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2023-01-11 17:33:50 +0100

    Update build-linux.sh

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -11,8 +11,7 @@ rm -rf "${sentryNativeRoot}/build"
 # See discussion in https://github.com/getsentry/sentry-unreal/pull/173
 docker run --rm dockcross/manylinux_2_28-x64 >./dockcross
 chmod +x ./dockcross
-./dockcross bash -c "cmake -S '${sentryNativeRoot}' -B '${sentryNativeRoot}/build' -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo"
-./dockcross bash -c "cmake --build '${sentryNativeRoot}/build' --target sentry --parallel"
+./dockcross bash -c "yum install -y openssl-devel && cmake -S '${sentryNativeRoot}' -B '${sentryNativeRoot}/build' -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo && cmake --build '${sentryNativeRoot}/build' --target sentry --parallel"
 
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
