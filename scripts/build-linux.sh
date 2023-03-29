52c9caec59805d56d8600cb470f0ec73d2ed96cc
bitsandfoxes <reg@bitfox.at>
2023-03-29 10:56:04 +0200

    forgot to actually build the SDK

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -5,7 +5,9 @@ export sentryNativeRoot=$1
 export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
-rm -rf "${sentryNativeRoot}/build"
+
+cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
 
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
