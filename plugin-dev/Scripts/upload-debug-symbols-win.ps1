b7706a102321d5e497891ad80bcbc18638aaff47
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-04-20 10:19:04 +0300

    Remove non-zero exit code when sentry.properties file is missing

--- plugin-dev/Scripts/upload-debug-symbols-win.ps1
+++ plugin-dev/Scripts/upload-debug-symbols-win.ps1
@@ -94,8 +94,8 @@ $PropertiesFile = "$ProjectPath/sentry.properties"
 
 If (-not (Test-Path -Path $PropertiesFile -PathType Leaf))
 {
-    Write-Error "Sentry: Properties file is missing: '$PropertiesFile'"
-    Exit 1
+    Write-Warning "Sentry: Properties file is missing: '$PropertiesFile'"
+    Exit
 }
 
 Write-Host "Sentry: Upload started using PropertiesFile '$PropertiesFile'"
