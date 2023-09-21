19f734e154b871a8da3ff85a695cc0e8d6902d69
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-09-21 13:16:06 +0300

    Add symbol uploading for Win

--- sample/Source/SentryPlaygroundEditor.Target.cs
+++ sample/Source/SentryPlaygroundEditor.Target.cs
@@ -8,15 +8,21 @@ public class SentryPlaygroundEditorTarget : TargetRules
 	public SentryPlaygroundEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		//DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		bCompileISPC = true;
+		bOverrideBuildEnvironment = true;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+
 		ExtraModuleNames.AddRange( new string[] { "SentryPlayground" } );
 
-		bOverrideBuildEnvironment = true;
-
-		if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion < 2)
-		{
-			bUsePCHFiles = false;
-			bUseUnityBuild = false;
-		}
+		// bOverrideBuildEnvironment = true;
+		//
+		// if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion < 2)
+		// {
+		// 	bUsePCHFiles = false;
+		// 	bUseUnityBuild = false;
+		// }
 	}
 }
