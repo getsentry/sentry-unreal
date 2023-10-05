60fb0b8c786213ed1babd19f59baa1c820092a82
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-10-05 13:56:04 +0300

    Add check of enabled platform/config during sym upload for Windows

--- plugin-dev/Scripts/upload-debug-symbols-win.bat
+++ plugin-dev/Scripts/upload-debug-symbols-win.bat
@@ -5,8 +5,9 @@ setlocal enabledelayedexpansion
 set TargetPlatform=%1
 set TargetName=%2
 set TargetType=%3
-set ProjectPath=%4
-set PluginPath=%5
+set TargetConfig=%4
+set ProjectPath=%5
+set PluginPath=%6
 
 set ProjectBinariesPath=%ProjectPath:"=%\Binaries\%TargetPlatform%
 set PluginBinariesPath=%PluginPath:"=%\Source\ThirdParty\%TargetPlatform%
@@ -20,6 +21,7 @@ if "%TargetType%"=="Editor" (
 )
 
 if "%TargetPlatform%"=="Win64" (
+    set "TargetPlatform=Windows"
     set CliExec=%PluginPath%\Source\ThirdParty\CLI\sentry-cli-Windows-x86_64.exe
 ) else if "%TargetPlatform%"=="Linux" (
     set CliExec=%PluginPath%\Source\ThirdParty\CLI\sentry-cli-Windows-x86_64.exe
@@ -46,6 +48,36 @@ if /i "%IncludeSourceFiles%"=="True" (
     set "CliArgs=--include-sources"
 )
 
+call :ParseIniFile "%ConfigPath%\DefaultEngine.ini" /Script/Sentry.SentrySettings EnableBuildPlatforms EnabledPlatforms
+if not "%EnabledPlatforms%"=="" (
+  set PlatformToCheck="bEnable%TargetPlatform%=False"
+  call :FindString EnabledPlatforms PlatformToCheck IsPlatformDisabled
+  if "!IsPlatformDisabled!"=="true" (
+      echo "Sentry: Automatic symbols upload is disabled for build platform %TargetPlatform%. Skipping..."
+      exit
+  )
+)
+
+call :ParseIniFile "%ConfigPath%\DefaultEngine.ini" /Script/Sentry.SentrySettings EnableBuildTargets EnabledTargets
+if not "%EnabledTargets%"=="" (
+  set TargetToCheck="bEnable%TargetType%=False"
+  call :FindString EnabledTargets TargetToCheck IsTargetDisabled
+  if "!IsTargetDisabled!"=="true" (
+      echo "Sentry: Automatic symbols upload is disabled for build target type %TargetType%. Skipping..."
+      exit
+  )
+)
+
+call :ParseIniFile "%ConfigPath%\DefaultEngine.ini" /Script/Sentry.SentrySettings EnableBuildConfigurations EnabledConfigurations
+if not "%EnabledConfigurations%"=="" (
+  set ConfigToCheck="bEnable%TargetConfig%=False"
+  call :FindString EnabledConfigurations ConfigToCheck IsConfigDisabled
+  if "!IsConfigDisabled!"=="true" (
+      echo "Sentry: Automatic symbols upload is disabled for build configuration %TargetConfig%. Skipping..."
+      exit
+  )
+)
+
 set PropertiesFile=%ProjectPath:"=%\sentry.properties
 
 if not exist "%PropertiesFile%" (
@@ -69,6 +101,19 @@ echo Sentry: Upload finished
 
 endlocal
 exit
+
+:FindString <sourceStr> <findStr> <result>
+  setlocal  
+  for /f "delims=" %%A in ('echo %%%1%%') do set str1=%%A
+  for /f "delims=" %%A in ('echo %%%2%%') do set str2=%%A
+  echo.%str1%|findstr /C:"%str2%" >nul 2>&1
+  endlocal
+  if not errorlevel 1 (
+    set %~3=true
+  ) else (
+    set %~3=false
+  )  
+  goto :eof
 
 :ParseIniFile <filename> <section> <key> <result>
   set %~4=
@@ -98,3 +143,5 @@ exit
     )
   )
   endlocal
+  set insection=
+  goto :eof
