0a252c715e8380b60509ca0227e1ed679e5c7536
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-10-17 11:14:23 +0300

    Fix crash utility function

--- sample/Source/SentryPlayground/SentryPlaygroundUtils.cpp
+++ sample/Source/SentryPlayground/SentryPlaygroundUtils.cpp
@@ -2,19 +2,12 @@
 
 void USentryPlaygroundUtils::Crash()
 {
-	UE_LOG(LogTemp, Log, TEXT("USentryPlaygroundUtils::Crash() utility function called!"));
-
-	// Supressing warnings for when using warnings as errors on the target. 
-	#pragma warning(suppress: 6011)	
-	char *ptr = 0;
-	#pragma warning(suppress: 6011)
+	volatile char *ptr = 0;
 	*ptr += 1;
 }
 
 void USentryPlaygroundUtils::Assert()
 {
-	UE_LOG(LogTemp, Log, TEXT("USentryPlaygroundUtils::Assert() utility function called!"));
-
 	char *ptr = nullptr;
 	check(ptr != nullptr);
 }
