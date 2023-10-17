a2db468949aeb47db1c11bc54fd2acaf916e7b06
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-10-17 15:58:52 +0300

    Add check if event has level

--- plugin-dev/Source/Sentry/Private/Desktop/SentryScopeDesktop.cpp
+++ plugin-dev/Source/Sentry/Private/Desktop/SentryScopeDesktop.cpp
@@ -172,10 +172,12 @@ void SentryScopeDesktop::Apply(USentryEvent* event)
 
 	sentry_value_t nativeEvent = eventDesktop->GetNativeObject();
 
-	FString levelStr = SentryConvertorsDesktop::SentryLevelToString(LevelDesktop).ToLower();
-	if (!levelStr.IsEmpty())
+	sentry_value_t eventLevel = sentry_value_get_by_key(nativeEvent, "level");
+
+	FString scopeLevelStr = SentryConvertorsDesktop::SentryLevelToString(LevelDesktop).ToLower();
+	if (!scopeLevelStr.IsEmpty() && sentry_value_is_null(eventLevel))
 	{
-		sentry_value_set_by_key(nativeEvent, "level", sentry_value_new_string(TCHAR_TO_ANSI(*levelStr)));
+		sentry_value_set_by_key(nativeEvent, "level", sentry_value_new_string(TCHAR_TO_ANSI(*scopeLevelStr)));
 	}
 
 	if(!Dist.IsEmpty())
