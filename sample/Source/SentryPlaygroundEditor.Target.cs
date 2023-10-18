1ab2441eeb0a8f8a1466102f2e9db6bf982f8799
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-10-18 15:31:31 +0300

    Upgrade sample to UE 5.1

--- sample/Source/SentryPlaygroundEditor.Target.cs
+++ sample/Source/SentryPlaygroundEditor.Target.cs
@@ -8,15 +8,8 @@ public class SentryPlaygroundEditorTarget : TargetRules
 	public SentryPlaygroundEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.AddRange( new string[] { "SentryPlayground" } );
-
-		bOverrideBuildEnvironment = true;
-
-		if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion < 2)
-		{
-			bUsePCHFiles = false;
-			bUseUnityBuild = false;
-		}
 	}
 }
