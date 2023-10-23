3d04b436d5fb8584defb8f135dc62c047660666b
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-10-23 10:55:20 +0300

    Fix artifacts path

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -8,7 +8,7 @@ rm -rf "${sentryArtifactsDestination}/"*
 
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D CMAKE_BUILD_TYPE=RelWithDebInfo -D SENTRY_BUILD_SHARED_LIBS=OFF
 cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
-cmake --install "${sentryNativeRoot}/build" --prefix install
+cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install"
 
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
