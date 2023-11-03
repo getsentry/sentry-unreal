d4a79fb24114ae09207f4c1a08c2cd3dcfe3cdd4
Ivan Tustanivskyi <tustanivsky@gmail.com>
2023-11-03 14:25:33 +0200

    Fix

--- scripts/build-win64-crashpad.sh
+++ scripts/build-win64-crashpad.sh
@@ -6,7 +6,7 @@ export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
 
-cmake -G "Visual Studio 16 2019" -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF
+cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config RelWithDebInfo --parallel
 cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install" --config RelWithDebInfo
