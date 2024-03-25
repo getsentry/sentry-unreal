bc62c993ceea8df7de43b67f4ecdf1f4635ce764
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-03-25 09:08:11 +0200

    Add breadcrumbs automatically when printing to logs

---
+++ plugin-dev/Source/Sentry/Private/SentryOutputDevice.cpp
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2024 Sentry. All Rights Reserved.
+
+#include "SentryOutputDevice.h"
+
+#include "SentryBreadcrumb.h"
+#include "SentryModule.h"
+#include "SentrySettings.h"
+#include "SentrySubsystem.h"
+
+void FSentryOutputDevice::Serialize(const TCHAR* V, ELogVerbosity::Type Verbosity, const FName& Category)
+{
+	const USentrySettings* Settings = FSentryModule::Get().GetSettings();
+
+	bool bAddBreadcrumb;
+
+	ESentryLevel BreadcrumbLevel = ESentryLevel::Debug;
+
+	switch (Verbosity)
+	{
+	case ELogVerbosity::Fatal:
+		bAddBreadcrumb = Settings->AutomaticBreadcrumbsForLogs.bOnFatalLog;
+		BreadcrumbLevel = ESentryLevel::Fatal;
+		break;
+	case ELogVerbosity::Error:
+		bAddBreadcrumb = Settings->AutomaticBreadcrumbsForLogs.bOnErrorLog;
+		BreadcrumbLevel = ESentryLevel::Error;
+		break;
+	case ELogVerbosity::Warning:
+		bAddBreadcrumb = Settings->AutomaticBreadcrumbsForLogs.bOnWarningLog;
+		BreadcrumbLevel = ESentryLevel::Warning;
+		break;
+	case ELogVerbosity::Display:
+	case ELogVerbosity::Log:
+		bAddBreadcrumb = Settings->AutomaticBreadcrumbsForLogs.bOnInfoLog;
+		BreadcrumbLevel = ESentryLevel::Info;
+		break;
+	case ELogVerbosity::Verbose:
+	case ELogVerbosity::VeryVerbose:
+		bAddBreadcrumb = Settings->AutomaticBreadcrumbsForLogs.bOnDebugLog;
+		BreadcrumbLevel = ESentryLevel::Debug;
+		break;
+	default:
+		bAddBreadcrumb = false;
+	}
+
+	if(!bAddBreadcrumb)
+	{
+		return;
+	}
+
+	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
+	if(!SentrySubsystem || !SentrySubsystem->IsEnabled())
+	{
+		return;
+	}
+
+	SentrySubsystem->AddBreadcrumbWithParams(V, Category.ToString(), FString(), TMap<FString, FString>(), BreadcrumbLevel);
+}
+
+bool FSentryOutputDevice::CanBeUsedOnAnyThread() const
+{
+	return true;
+}
+
+bool FSentryOutputDevice::CanBeUsedOnMultipleThreads() const
+{
+	return true;
+}
+
+bool FSentryOutputDevice::CanBeUsedOnPanicThread() const
+{
+	return true;
+}
