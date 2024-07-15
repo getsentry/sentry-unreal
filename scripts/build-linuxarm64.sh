37333e71941073861c0f84f574bebe4377d9baa5
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-07-15 11:00:35 +0300

    Test
    Change file mode
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Tezt
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test
    
    Test flags
    
    Test
    
    Test
    
    Test

--- scripts/build-linuxarm64.sh
+++ scripts/build-linuxarm64.sh
@@ -4,10 +4,28 @@ set -euo pipefail
 export sentryNativeRoot=$1
 export sentryArtifactsDestination=$2
 
+TOOLCHAIN_URL="http://cdn.unrealengine.com/Toolchain_Linux/native-linux-v22_clang-16.0.6-centos7.tar.gz"
+TOOLCHAIN_ARCHIVE="sentry-toolchain"
+
+TOOLCHAIN_DIR="sentry-toolchain-test"
+mkdir $TOOLCHAIN_DIR
+
+wget $TOOLCHAIN_URL -O ${TOOLCHAIN_ARCHIVE}.tmp
+mv ${TOOLCHAIN_ARCHIVE}.tmp $TOOLCHAIN_ARCHIVE
+
+tar -xvf $TOOLCHAIN_ARCHIVE -C "sentry-toolchain-test"
+
+# Set environment variables for the toolchain (modify based on your needs)
+export PATH=/home/runner/work/sentry-unreal/sentry-unreal/sentry-toolchain-test/v22_clang-16.0.6-centos7/aarch64-unknown-linux-gnueabi/bin:$PATH
+export CXX=/home/runner/work/sentry-unreal/sentry-unreal/sentry-toolchain-test/v22_clang-16.0.6-centos7/aarch64-unknown-linux-gnueabi/bin/clang++
+export CC=/home/runner/work/sentry-unreal/sentry-unreal/sentry-toolchain-test/v22_clang-16.0.6-centos7/aarch64-unknown-linux-gnueabi/bin/clang
+
 rm -rf "${sentryArtifactsDestination}/"*
 
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
-    -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER="clang++" -D CMAKE_CXX_FLAGS="-stdlib=libc++" -D CMAKE_EXE_LINKER_FLAGS="-stdlib=libc++" -D CMAKE_SYSTEM_PROCESSOR=aarch64
+    -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=$CC -D CMAKE_CXX_COMPILER=$CXX \
+    -D CMAKE_C_COMPILE_OPTIONS_SYSROOT="/home/runner/work/sentry-unreal/sentry-unreal/sentry-toolchain-test/v22_clang-16.0.6-centos7/aarch64-unknown-linux-gnueabi" \
+    -D CMAKE_CXX_COMPILE_OPTIONS_SYSROOT="/home/runner/work/sentry-unreal/sentry-unreal/sentry-toolchain-test/v22_clang-16.0.6-centos7/aarch64-unknown-linux-gnueabi"
 cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
 cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install"
 
