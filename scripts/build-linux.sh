ecec67ba5caca857e6d12b1ea9365f8d24b469b7
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-09-04 14:39:47 +0300

    Test

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -7,7 +7,7 @@ export sentryArtifactsDestination=$2
 rm -rf "${sentryArtifactsDestination}/"*
 
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
-    -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=clang-16 -D CMAKE_CXX_COMPILER="clang++-16" -D CMAKE_CXX_LINKER="lld-16" -D CMAKE_CXX_FLAGS="-stdlib=libc++" -D CMAKE_EXE_LINKER_FLAGS="-stdlib=libc++"
+    -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=clang-16 -D CMAKE_CXX_COMPILER="clang++-16" -D CMAKE_CXX_FLAGS="-stdlib=libc++ -std=c++20 -fuse-ld=lld" -D CMAKE_EXE_LINKER_FLAGS="-stdlib=libc++"
 cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
 cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install"
 
