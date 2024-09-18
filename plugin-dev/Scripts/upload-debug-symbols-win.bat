e33f7681bb211c5bbee2b45da411ed9abba86f9e
Elliot Prior <1709938+Quogu@users.noreply.github.com>
2024-09-18 15:43:44 +0100

    Restructure, as the echo of the plugin settings value doesn't work if it's inside the else block for some reason.

--- plugin-dev/Scripts/upload-debug-symbols-win.bat
+++ plugin-dev/Scripts/upload-debug-symbols-win.bat
@@ -32,12 +32,12 @@ if "%TargetPlatform%"=="Win64" (
     exit /B 0
 )
 
+call :ParseIniFile "%ConfigPath%\DefaultEngine.ini" /Script/Sentry.SentrySettings UploadSymbolsAutomatically UploadSymbols
+echo Sentry: Automatic symbols upload is '%UploadSymbols%' from plugin settings
+
 if /i "%SENTRY_UPLOAD_SYMBOLS_AUTOMATICALLY%" NEQ "" (
   set UploadSymbols=%SENTRY_UPLOAD_SYMBOLS_AUTOMATICALLY%
   echo Sentry: Automatic symbols upload is overridden as '%SENTRY_UPLOAD_SYMBOLS_AUTOMATICALLY%' from environment variable SENTRY_UPLOAD_SYMBOLS_AUTOMATICALLY
-) else (
-  call :ParseIniFile "%ConfigPath%\DefaultEngine.ini" /Script/Sentry.SentrySettings UploadSymbolsAutomatically UploadSymbols
-  echo Sentry: Automatic symbols upload is '%UploadSymbols%' from plugin settings
 )
 
 if /i "%UploadSymbols%" NEQ "True" (
