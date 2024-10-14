6d49930d5d252921be0610e3b9e4d755afc7326d
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-10-14 13:48:25 +0300

    Update comments

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -6,19 +6,19 @@ export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
 
-# Build sentry-native using libc++
+# Build sentry-native using clang and libc++ for static libs
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER="clang++" -D CMAKE_CXX_FLAGS="-stdlib=libc++" -D CMAKE_EXE_LINKER_FLAGS="-stdlib=libc++"
 cmake --build "${sentryNativeRoot}/build" --target sentry --parallel
 cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install"
 
-# Build sentry-native using libstdc++
+# Build sentry-native using gcc for crashpad which's not depending on libc++
 cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build_crashpad_handler" -D SENTRY_BACKEND=crashpad -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build "${sentryNativeRoot}/build_crashpad_handler" --target sentry --parallel
 cmake --install "${sentryNativeRoot}/build_crashpad_handler" --prefix "${sentryNativeRoot}/install_crashpad_handler"
 
-# Replace crashpad_handler binary 
+# Replace crashpad_handler obtained after the first build with the matching binary from the second one
 cp "${sentryNativeRoot}/install_crashpad_handler/bin/crashpad_handler" "${sentryNativeRoot}/install/bin/crashpad_handler"
 
 mkdir "${sentryArtifactsDestination}/bin"
