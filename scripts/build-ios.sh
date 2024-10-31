fd8a4e3f678efa996fa4b6792245c504d4323d02
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-10-31 11:35:19 +0200

    Optimize build times

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -7,10 +7,27 @@ export sentryArtifactsDestination=$2
 rm -rf "${sentryArtifactsDestination}/"*
 
 pushd "${sentryIosRoot}"
-./scripts/build-xcframework.sh iOSOnly
+
+xcodebuild archive \
+    -project Sentry.xcodeproj/ \
+    -scheme "Sentry" \
+    -configuration "Release" \
+    -sdk "iphoneos" \
+    -archivePath "./Carthage/archive/Sentry/iphoneos.xcarchive" \
+    CODE_SIGNING_REQUIRED=NO \
+    SKIP_INSTALL=NO \
+    CODE_SIGN_IDENTITY= \
+    CARTHAGE=YES \
+    MACH_O_TYPE="mh_dylib" \
+    ENABLE_CODE_COVERAGE=NO \
+    GCC_GENERATE_DEBUGGING_SYMBOLS="YES" \
+    OTHER_LDFLAGS="-Wl,-make_mergeable"
+
+xcodebuild -create-xcframework -framework Carthage/archive/Sentry/iphoneos.xcarchive/Products/Library/Frameworks/Sentry.framework 
+
 popd
 
-cp -R "${sentryIosRoot}/Carthage/Sentry-Dynamic.xcframework/ios-arm64_arm64e/Sentry.framework" "${sentryArtifactsDestination}/Sentry.framework"
+cp -R "${sentryIosRoot}/Carthage/Sentry.xcframework/ios-arm64_arm64e/Sentry.framework" "${sentryArtifactsDestination}/Sentry.framework"
 
 mkdir "Sentry.embeddedframework"
 
--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -7,7 +7,24 @@ export sentryArtifactsDestination=$2
 rm -rf "${sentryArtifactsDestination}/"*
 
 pushd "${sentryCocoaRoot}"
-./scripts/build-xcframework.sh gameOnly
+
+xcodebuild archive \
+    -project Sentry.xcodeproj/ \
+    -scheme "Sentry" \
+    -configuration "Release" \
+    -sdk "macosx" \
+    -archivePath "./Carthage/archive/Sentry/macosx.xcarchive" \
+    CODE_SIGNING_REQUIRED=NO \
+    SKIP_INSTALL=NO \
+    CODE_SIGN_IDENTITY= \
+    CARTHAGE=YES \
+    MACH_O_TYPE="mh_dylib" \
+    ENABLE_CODE_COVERAGE=NO \
+    GCC_GENERATE_DEBUGGING_SYMBOLS="YES" \
+    OTHER_LDFLAGS="-Wl,-make_mergeable"
+
+xcodebuild -create-xcframework -framework Carthage/archive/Sentry/macosx.xcarchive/Products/Library/Frameworks/Sentry.framework 
+
 popd
 
 mkdir "${sentryArtifactsDestination}/bin"
