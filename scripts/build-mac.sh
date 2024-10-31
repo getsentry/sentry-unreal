cae3dcac1a45912eb902162c350c2f7fe2dafda7
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-10-31 11:43:38 +0200

    Fix path for Mac

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -30,7 +30,7 @@ popd
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
 
-cp "${sentryCocoaRoot}/Carthage/Sentry-Dynamic.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry" "${sentryArtifactsDestination}/bin/sentry.dylib"
+cp "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry" "${sentryArtifactsDestination}/bin/sentry.dylib"
 
-cp -rL "${sentryCocoaRoot}/Carthage/Sentry-Dynamic.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry/Headers" "${sentryArtifactsDestination}/include/Sentry"
-cp -rL "${sentryCocoaRoot}/Carthage/Sentry-Dynamic.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry/PrivateHeaders/." "${sentryArtifactsDestination}/include/Sentry"
+cp -rL "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry/Headers" "${sentryArtifactsDestination}/include/Sentry"
+cp -rL "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry/PrivateHeaders/." "${sentryArtifactsDestination}/include/Sentry"
