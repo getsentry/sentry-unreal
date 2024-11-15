70b4603e4ae390438addeae3fa4e669f95dfae8a
Ivan Tustanivskyi <tustanivsky@gmail.com>
2024-11-15 15:16:16 +0200

    Fix

--- plugin-dev/Source/Sentry/Private/Tests/SentryTests.h
+++ plugin-dev/Source/Sentry/Private/Tests/SentryTests.h
@@ -6,7 +6,7 @@
 #include "Misc/EngineVersionComparison.h"
 
 #if UE_VERSION_OLDER_THAN(5, 4, 0)
-constexpr EAutomationTestFlags::Type SentryApplicationContextMask = EAutomationTestFlags::ApplicationContextMask;
+static constexpr EAutomationTestFlags::Type SentryApplicationContextMask = EAutomationTestFlags::ApplicationContextMask;
 #else
-inline constexpr EAutomationTestFlags SentryApplicationContextMask = EAutomationTestFlags_ApplicationContextMask;
+static constexpr EAutomationTestFlags SentryApplicationContextMask = EAutomationTestFlags_ApplicationContextMask;
 #endif
