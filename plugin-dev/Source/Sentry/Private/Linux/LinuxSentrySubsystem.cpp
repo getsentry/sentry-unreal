a08903f7bc81f76dbd5fdb7771b6d88787f90045
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-02-03 16:28:21 +0200

    Fix compilation errors on Linux (#4)
    * Fix compilation errors on Linux
    
    * Add missing includes
    
    * Fix demo blueprint engine version

--- plugin-dev/Source/Sentry/Private/Linux/LinuxSentrySubsystem.cpp
+++ plugin-dev/Source/Sentry/Private/Linux/LinuxSentrySubsystem.cpp
@@ -1,26 +1,29 @@
 #include "LinuxSentrySubsystem.h"
 
+#include "GenericPlatform/GenericPlatformOutputDevices.h"
+#include "Misc/Paths.h"
+
 #if USE_SENTRY_NATIVE
 
 void FLinuxSentrySubsystem::ConfigureHandlerPath(sentry_options_t* Options)
 {
-	sentry_options_set_handler_path(options, TCHAR_TO_UTF8(*GetHandlerPath()));
+	sentry_options_set_handler_path(Options, TCHAR_TO_UTF8(*GetHandlerPath()));
 }
 
 void FLinuxSentrySubsystem::ConfigureDatabasePath(sentry_options_t* Options)
 {
-	sentry_options_set_database_path(options, TCHAR_TO_UTF8(*GetDatabasePath()));
+	sentry_options_set_database_path(Options, TCHAR_TO_UTF8(*GetDatabasePath()));
 }
 
 void FLinuxSentrySubsystem::ConfigureLogFileAttachment(sentry_options_t* Options)
 {
 	const FString LogFilePath = FGenericPlatformOutputDevices::GetAbsoluteLogFilename();
-	sentry_options_add_attachment(options, TCHAR_TO_UTF8(*FPaths::ConvertRelativePathToFull(LogFilePath)));
+	sentry_options_add_attachment(Options, TCHAR_TO_UTF8(*FPaths::ConvertRelativePathToFull(LogFilePath)));
 }
 
 void FLinuxSentrySubsystem::ConfigureScreenshotAttachment(sentry_options_t* Options)
 {
-	sentry_options_add_attachment(options, TCHAR_TO_UTF8(*GetScreenshotPath()));
+	sentry_options_add_attachment(Options, TCHAR_TO_UTF8(*GetScreenshotPath()));
 }
 
 void FLinuxSentrySubsystem::ConfigureGpuDumpAttachment(sentry_options_t* Options)
