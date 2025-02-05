7252e28b7d3ea020a97b9d649be00bb3dbed90f7
Nathan White <nwhite@riotgames.com>
2025-02-04 19:22:15 -0800

    Reparent Xbox subsystem to Microsoft

--- plugin-dev/Source/Sentry/Private/Xbox/XboxSentrySubsystem.h
+++ plugin-dev/Source/Sentry/Private/Xbox/XboxSentrySubsystem.h
@@ -1,10 +1,15 @@
 #pragma once
 
-#include "GenericPlatform/GenericPlatformSentrySubsystem.h"
+#if USE_SENTRY_NATIVE
 
-class FXboxSentrySubsystem : public FGenericPlatformSentrySubsystem
+#include "Microsoft/MicrosoftSentrySubsystem.h"
+
+class FXboxSentrySubsystem : public FMicrosoftSentrySubsystem
 {
-
+public:
+	virtual void ConfigureHandlerPath(sentry_options_t* Options) override {}
 };
 
 typedef FXboxSentrySubsystem FPlatformSentrySubsystem;
+
+#endif // USE_SENTRY_NATIVE
