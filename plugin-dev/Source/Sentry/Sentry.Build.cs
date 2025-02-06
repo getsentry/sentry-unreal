bf47b71c7b805f8f41922b1383efb45d48a3a326
PlasmaDev5 <dragpmCASTjosh@gmail.com>
2025-02-06 08:06:46 +0000

    Make cmake optional on windows

--- plugin-dev/Source/Sentry/Sentry.Build.cs
+++ plugin-dev/Source/Sentry/Sentry.Build.cs
@@ -17,6 +17,9 @@ using Tools.DotNETCommon;
 
 public class Sentry : ModuleRules
 {
+	[CommandLine("-buildNativeForWindows")]
+	public bool bBuildNativeForWindows = false;
+	
 	public Sentry(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -79,113 +82,147 @@ public class Sentry : ModuleRules
 			
 			PublicDefinitions.Add("USE_SENTRY_NATIVE=1");
 			PublicDefinitions.Add("SENTRY_BUILD_STATIC=1");
-			
-			var targetLocation = Path.Combine(PluginDirectory, "sentry-native");
 
-			CMakeTargetInst cmakeTarget =
-				new CMakeTargetInst("sentry-native", Target.Platform.ToString(), targetLocation, "");
-			cmakeTarget.Load(Target, this);
-
-			string intermediatePath =
-				Path.Combine(Target.ProjectFile.Directory.FullName, "Intermediate", "CMakeTarget", "sentry-native");
-				
-			Console.WriteLine("Adding include path: "+targetLocation+"/include");
-			PublicIncludePaths.Add(targetLocation + "/include");
-			
-			string buildOutputPath = Path.Combine(PluginDirectory, "Binaries", "Win64");
-					
-			string buildPath = Path.Combine(intermediatePath, "Win64", "build");
-			if(Target.Configuration == UnrealTargetConfiguration.Debug)
+			if (bBuildNativeForWindows)
 			{
-				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Debug", "sentry.lib"));
-			}
-			else
-			{ 
-				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Release", "sentry.lib"));
-			}
+				var targetLocation = Path.Combine(PluginDirectory, "sentry-native");
 
-			if (!PublicDefinitions.Contains("USE_SENTRY_BREAKPAD=1"))
-			{
-				string crashpadBuildPath = Path.Combine(buildPath, "crashpad_build");
+				CMakeTargetInst cmakeTarget =
+					new CMakeTargetInst("sentry-native", Target.Platform.ToString(), targetLocation, "");
+				cmakeTarget.Load(Target, this);
+
+				string intermediatePath =
+					Path.Combine(Target.ProjectFile.Directory.FullName, "Intermediate", "CMakeTarget", "sentry-native");
+
+				Console.WriteLine("Adding include path: " + targetLocation + "/include");
+				PublicIncludePaths.Add(targetLocation + "/include");
+
+				string buildOutputPath = Path.Combine(PluginDirectory, "Binaries", "Win64");
+
+				string buildPath = Path.Combine(intermediatePath, "Win64", "build");
 				if (Target.Configuration == UnrealTargetConfiguration.Debug)
 				{
-					if (!File.Exists(Path.Combine(buildOutputPath, "crashpad_handler.exe")))
-					{
-						Console.WriteLine("Copying crashpad_handler.exe");
-						if (!System.IO.Directory.Exists(buildOutputPath))
-						{
-							System.IO.Directory.CreateDirectory(buildOutputPath);
-						}
-
-						File.Copy(Path.Combine(crashpadBuildPath, "handler", "Debug", "crashpad_handler.exe"),
-							Path.Combine(buildOutputPath, "crashpad_handler.exe"));
-					}
-					else
-					{
-						Console.WriteLine("crashpad_handler.exe already exists");
-					}
-
-					RuntimeDependencies.Add(Path.Combine(buildOutputPath, "crashpad_handler.exe"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "handler", "Debug",
-						"crashpad_handler_lib.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "client", "Debug",
-						"crashpad_client.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "compat", "Debug",
-						"crashpad_compat.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "minidump", "Debug",
-						"crashpad_minidump.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "snapshot", "Debug",
-						"crashpad_snapshot.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "getopt", "Debug",
-						"crashpad_getopt.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "mini_chromium",
-						"Debug", "mini_chromium.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "zlib", "Debug",
-						"crashpad_zlib.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "tools", "Debug",
-						"crashpad_tools.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "util", "Debug",
-						"crashpad_util.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Debug", "sentry.lib"));
 				}
 				else
 				{
-					if (!File.Exists(Path.Combine(buildOutputPath, "crashpad_handler.exe")))
+					PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Release", "sentry.lib"));
+				}
+
+				if (!PublicDefinitions.Contains("USE_SENTRY_BREAKPAD=1"))
+				{
+					string crashpadBuildPath = Path.Combine(buildPath, "crashpad_build");
+					if (Target.Configuration == UnrealTargetConfiguration.Debug)
 					{
-						Console.WriteLine("Copying crashpad_handler.exe");
-						if (!System.IO.Directory.Exists(buildOutputPath))
+						if (!File.Exists(Path.Combine(buildOutputPath, "crashpad_handler.exe")))
 						{
-							System.IO.Directory.CreateDirectory(buildOutputPath);
+							Console.WriteLine("Copying crashpad_handler.exe");
+							if (!System.IO.Directory.Exists(buildOutputPath))
+							{
+								System.IO.Directory.CreateDirectory(buildOutputPath);
+							}
+
+							File.Copy(Path.Combine(crashpadBuildPath, "handler", "Debug", "crashpad_handler.exe"),
+								Path.Combine(buildOutputPath, "crashpad_handler.exe"));
+						}
+						else
+						{
+							Console.WriteLine("crashpad_handler.exe already exists");
 						}
 
-						File.Copy(Path.Combine(crashpadBuildPath, "handler", "Release", "crashpad_handler.exe"),
-							Path.Combine(buildOutputPath, "crashpad_handler.exe"));
+						RuntimeDependencies.Add(Path.Combine(buildOutputPath, "crashpad_handler.exe"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "handler", "Debug",
+							"crashpad_handler_lib.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "client", "Debug",
+							"crashpad_client.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "compat", "Debug",
+							"crashpad_compat.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "minidump", "Debug",
+							"crashpad_minidump.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "snapshot", "Debug",
+							"crashpad_snapshot.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "getopt", "Debug",
+							"crashpad_getopt.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "mini_chromium",
+							"Debug", "mini_chromium.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "zlib", "Debug",
+							"crashpad_zlib.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "tools", "Debug",
+							"crashpad_tools.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "util", "Debug",
+							"crashpad_util.lib"));
 					}
 					else
 					{
-						Console.WriteLine("crashpad_handler.exe already exists");
-					}
+						if (!File.Exists(Path.Combine(buildOutputPath, "crashpad_handler.exe")))
+						{
+							Console.WriteLine("Copying crashpad_handler.exe");
+							if (!System.IO.Directory.Exists(buildOutputPath))
+							{
+								System.IO.Directory.CreateDirectory(buildOutputPath);
+							}
 
-					RuntimeDependencies.Add(Path.Combine(buildOutputPath, "crashpad_handler.exe"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "handler", "Release",
-						"crashpad_handler_lib.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "client", "Release",
-						"crashpad_client.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "compat", "Release",
-						"crashpad_compat.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "minidump", "Release",
-						"crashpad_minidump.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "snapshot", "Release",
-						"crashpad_snapshot.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "getopt", "Release",
-						"crashpad_getopt.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "mini_chromium",
-						"Release", "mini_chromium.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "zlib", "Release",
-						"crashpad_zlib.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "tools", "Release",
-						"crashpad_tools.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "util", "Release",
-						"crashpad_util.lib"));
+							File.Copy(Path.Combine(crashpadBuildPath, "handler", "Release", "crashpad_handler.exe"),
+								Path.Combine(buildOutputPath, "crashpad_handler.exe"));
+						}
+						else
+						{
+							Console.WriteLine("crashpad_handler.exe already exists");
+						}
+
+						RuntimeDependencies.Add(Path.Combine(buildOutputPath, "crashpad_handler.exe"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "handler", "Release",
+							"crashpad_handler_lib.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "client", "Release",
+							"crashpad_client.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "compat", "Release",
+							"crashpad_compat.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "minidump", "Release",
+							"crashpad_minidump.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "snapshot", "Release",
+							"crashpad_snapshot.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "getopt",
+							"Release",
+							"crashpad_getopt.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "mini_chromium",
+							"Release", "mini_chromium.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "third_party", "zlib", "Release",
+							"crashpad_zlib.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "tools", "Release",
+							"crashpad_tools.lib"));
+						PublicAdditionalLibraries.Add(Path.Combine(crashpadBuildPath, "util", "Release",
+							"crashpad_util.lib"));
+					}
+				}
+			}
+			else
+			{
+				bool CrashpadExists = File.Exists(Path.Combine(PlatformThirdPartyPath, "Crashpad", "bin", "crashpad_handler.exe"));
+
+				string WindowsThirdPartyPath = Path.Combine(PlatformThirdPartyPath, CrashpadExists ? "Crashpad" : "Breakpad");
+
+				PublicIncludePaths.Add(Path.Combine(WindowsThirdPartyPath, "include"));
+				PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private", "Windows"));
+
+				PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "sentry.lib"));
+
+				if (CrashpadExists)
+				{
+					RuntimeDependencies.Add(Path.Combine(PlatformBinariesPath, "crashpad_handler.exe"), Path.Combine(WindowsThirdPartyPath, "bin", "crashpad_handler.exe"));
+
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_compat.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_snapshot.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_util.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "mini_chromium.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_client.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_zlib.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_getopt.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_tools.lib"));
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "crashpad_handler_lib.lib"));
+				}
+				else
+				{
+					PublicAdditionalLibraries.Add(Path.Combine(WindowsThirdPartyPath, "lib", "breakpad_client.lib"));
 				}
 			}
 
@@ -255,14 +292,15 @@ public class Sentry : ModuleRules
 			if(Target.Configuration == UnrealTargetConfiguration.Debug)
 			{
 				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Debug", "sentry.lib"));
+				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Debug", "breakpad_client.lib"));
 			}
 			else
 			{ 
 				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Release", "sentry.lib"));
+				PublicAdditionalLibraries.Add(Path.Combine(buildPath, "Release", "breakpad_client.lib"));
 			}
 			
 			PublicSystemLibraries.Add("winhttp.lib");
-			PublicSystemLibraries.Add("version.lib");
 		}
 		else
 		{
