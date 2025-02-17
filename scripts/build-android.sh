94dbc03e86a3fe5e55e2c38fa6bb81960ef16cbf
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-02-17 12:45:53 +0200

    Download pre-built Native SDK NDK

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -16,3 +16,30 @@ cp "${sentryJavaRoot}/sentry-android-ndk/build/outputs/aar/sentry-android-ndk-re
 cp "${sentryJavaRoot}/sentry-android-core/build/outputs/aar/sentry-android-core-release.aar" "${sentryArtifactsDestination}/sentry-android-core-release.aar"
 
 cp "${sentryJavaRoot}/sentry/build/libs/"sentry-*.jar "${sentryArtifactsDestination}/sentry.jar"
+
+# With version v8 of the sentry-java the Native SDK NDK has to be downloaded separately from the sentry-native repo release page
+
+configFile="${sentryJavaRoot}/buildSrc/src/main/java/Config.kt"
+
+if [[ ! -f "$configFile" ]]; then
+  echo "Error: Config.kt file not found at $configFile"
+  exit 1
+fi
+
+nativeNdkVersion=$(grep 'sentryNativeNdk' "$configFile" | sed -E 's/.*sentryNativeNdk\s*=\s*"[^"]+:([^"]+)".*/\1/')
+
+if [[ -z "$nativeNdkVersion" ]]; then
+  echo "Error: Failed to extract Native SDK NDK version."
+  exit 1
+fi
+
+echo "Extracted Sentry Native NDK version: $nativeNdkVersion"
+
+nativeNdkUrl="https://github.com/getsentry/sentry-native/releases/download/${nativeNdkVersion}/sentry-native-ndk-${nativeNdkVersion}.zip"
+nativeNdkCache="${sentryJavaRoot}/native-ndk-cache"
+
+curl -L "${nativeNdkUrl}" -o "${nativeNdkCache}/sentry-native-ndk-${nativeNdkVersion}.zip"
+
+unzip -o "${nativeNdkCache}/sentry-native-ndk-${nativeNdkVersion}.zip" -d "${nativeNdkCache}/"
+
+cp "${nativeNdkCache}/sentry-native-ndk-${nativeNdkVersion}/sentry-native-ndk-release.aar" "${sentryArtifactsDestination}/sentry-native-ndk-release.aar"
