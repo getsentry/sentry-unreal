8b991915f38cbc6ee88df06dad4ec5a52faf4bba
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-05-22 19:39:52 +0300

    Update sample project settings (#918)
    * Enable .dSYM file generation for Mac/iOS
    
    * Set bundle name for modern xcode workflow
    
    * Add engine version check

--- sample/Source/SentryPlayground.Target.cs
+++ sample/Source/SentryPlayground.Target.cs
@@ -10,6 +10,11 @@ public class SentryPlaygroundTarget : TargetRules
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
+#if UE_5_0_OR_LATER
+		MacPlatform.bUseDSYMFiles = true;
+		IOSPlatform.bGeneratedSYM = true;
+#endif
+
 #if UE_5_1_OR_LATER
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 #endif
