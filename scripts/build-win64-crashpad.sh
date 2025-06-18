f67f346020ebbbfef350c7b1a2c89f2ed0878bce
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-06-18 15:43:26 +0300

    Update build-win64-crashpad.sh

--- scripts/build-win64-crashpad.sh
+++ scripts/build-win64-crashpad.sh
@@ -6,6 +6,7 @@ export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
 
+# Force CMake to use the VS2019 toolset (`-T v142`) to maintain native libraries compatibility with older Unreal Engine versions (4.27–5.1)
 cmake -G "Visual Studio 17 2022" -T v142 -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config RelWithDebInfo --parallel
