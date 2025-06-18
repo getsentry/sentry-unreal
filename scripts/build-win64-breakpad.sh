e4b90d4e30fe052a20d290ae8cb8489f5c760659
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-06-18 17:11:29 +0300

    chore: Update Windows runners for pre-building Native SDK in CI (#967)
    * Bump windows runner version
    
    * Bump VS version
    
    * Install Visual Studio 2019 build tools to build Native SDK
    
    * Update build-win64-breakpad.sh
    
    * Update build-win64-crashpad.sh

--- scripts/build-win64-breakpad.sh
+++ scripts/build-win64-breakpad.sh
@@ -6,7 +6,8 @@ export sentryArtifactsDestination=$2
 
 rm -rf "${sentryArtifactsDestination}/"*
 
-cmake -G "Visual Studio 16 2019" -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=breakpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF
+# Force CMake to use the VS2019 toolset (`-T v142`) to maintain native libraries compatibility with older Unreal Engine versions (4.27–5.1)
+cmake -G "Visual Studio 17 2022" -T v142 -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=breakpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --install "${sentryNativeRoot}/build" --prefix "${sentryNativeRoot}/install" --config RelWithDebInfo
 
