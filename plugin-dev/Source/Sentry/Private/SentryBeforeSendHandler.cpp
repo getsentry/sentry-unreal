06a7f0ef4410e48fa6c0aea03b5f0698f3408159
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-07-09 15:58:36 +0300

    Make Unreal callbacks invocation optional

--- plugin-dev/Source/Sentry/Private/SentryBeforeSendHandler.cpp
+++ plugin-dev/Source/Sentry/Private/SentryBeforeSendHandler.cpp
@@ -2,33 +2,10 @@
 
 #include "SentryBeforeSendHandler.h"
 
-#include "SentryAttachment.h"
 #include "SentryEvent.h"
 #include "SentryHint.h"
-#include "SentryLibrary.h"
-#include "SentryModule.h"
-#include "SentrySettings.h"
-
-#include "Utils/SentryFileUtils.h"
-
-#include "Misc/Paths.h"
 
 USentryEvent* USentryBeforeSendHandler::HandleBeforeSend_Implementation(USentryEvent* Event, USentryHint* Hint)
 {
-	const USentrySettings* Settings = FSentryModule::Get().GetSettings();
-
-	if (Settings->EnableAutoLogAttachment && Hint != nullptr)
-	{
-#if PLATFORM_ANDROID || PLATFORM_APPLE
-		const FString LogFilePath = Event->IsCrash() ? SentryFileUtils::GetGameLogBackupPath() : SentryFileUtils::GetGameLogPath();
-#else
-		const FString LogFilePath = SentryFileUtils::GetGameLogPath();
-#endif
-
-		Hint->AddAttachment(
-			USentryLibrary::CreateSentryAttachmentWithPath(
-				LogFilePath, FPaths::GetCleanFilename(LogFilePath), TEXT("text/plain")));
-	}
-
 	return Event;
 }
