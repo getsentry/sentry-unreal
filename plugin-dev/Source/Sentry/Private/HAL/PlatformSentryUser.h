77c5e54af72cc88af2fde4c54d81ebe23e489be1
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-07-16 14:31:55 +0300

    Remove platform-specific header stubs for `User` class

--- plugin-dev/Source/Sentry/Private/HAL/PlatformSentryUser.h
+++ plugin-dev/Source/Sentry/Private/HAL/PlatformSentryUser.h
@@ -2,6 +2,12 @@
 
 #pragma once
 
-#include "PlatformSentryDefines.h"
-
-#include SENTRY_COMPILED_PLATFORM_HEADER(SentryUser.h)
+#if PLATFORM_ANDROID
+#include "Android/AndroidSentryUser.h"
+#elif PLATFORM_APPLE
+#include "Apple/AppleSentryUser.h"
+#elif USE_SENTRY_NATIVE
+#include "GenericPlatform/GenericPlatformSentryUser.h"
+#else
+#include "Null/NullSentryUser.h"
+#endif
