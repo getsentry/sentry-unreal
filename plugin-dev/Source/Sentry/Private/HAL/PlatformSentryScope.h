51ed89b35f9f64db681b735bb26b95a3a5d27adf
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-07-16 10:15:55 +0300

    Clean up include conditions

--- plugin-dev/Source/Sentry/Private/HAL/PlatformSentryScope.h
+++ plugin-dev/Source/Sentry/Private/HAL/PlatformSentryScope.h
@@ -2,18 +2,14 @@
 
 #pragma once
 
-#ifdef SENTRY_PLATFORM_NULL
-#include "Null/NullSentryScope.h"
-#else
 #if PLATFORM_ANDROID
 #include "Android/AndroidSentryScope.h"
 #elif PLATFORM_APPLE
 #include "Apple/AppleSentryScope.h"
-#elif PLATFORM_MICROSOFT
+#elif USE_SENTRY_NATIVE && PLATFORM_MICROSOFT
 #include "Microsoft/MicrosoftSentryScope.h"
 #elif USE_SENTRY_NATIVE
 #include "GenericPlatform/GenericPlatformSentryScope.h"
 #else
 #include "Null/NullSentryScope.h"
-#endif
 #endif
