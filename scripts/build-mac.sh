61b703f2fd8cfb0b85c6ebdd8cea234154e00e63
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-07-18 11:22:49 +0300

    Test without UI Kit

--- scripts/build-mac.sh
+++ scripts/build-mac.sh
@@ -8,22 +8,22 @@ rm -rf "${sentryArtifactsDestination}/"*
 
 pushd "${sentryCocoaRoot}"
 
-# xcodebuild archive \
-#     -project Sentry.xcodeproj/ \
-#     -scheme "Sentry" \
-#     -configuration "Release" \
-#     -sdk "macosx" \
-#     -archivePath "./Carthage/archive/Sentry/macosx.xcarchive" \
-#     CODE_SIGNING_REQUIRED=NO \
-#     SKIP_INSTALL=NO \
-#     CODE_SIGN_IDENTITY= \
-#     CARTHAGE=YES \
-#     MACH_O_TYPE="mh_dylib" \
-#     ENABLE_CODE_COVERAGE=NO \
-#     GCC_GENERATE_DEBUGGING_SYMBOLS="YES" \
-#     OTHER_LDFLAGS="-Wl,-make_mergeable"
+xcodebuild archive \
+    -project Sentry.xcodeproj/ \
+    -scheme "Sentry" \
+    -configuration "ReleaseWithoutUIKit" \
+    -sdk "macosx" \
+    -archivePath "./Carthage/archive/Sentry/macosx.xcarchive" \
+    CODE_SIGNING_REQUIRED=NO \
+    SKIP_INSTALL=NO \
+    CODE_SIGN_IDENTITY= \
+    CARTHAGE=YES \
+    MACH_O_TYPE="staticlib" \
+    ENABLE_CODE_COVERAGE=NO \
+    GCC_GENERATE_DEBUGGING_SYMBOLS="YES" \
+    OTHER_LDFLAGS=""
 
-./scripts/build-xcframework-local.sh macOSOnly StaticOnly
+# ./scripts/build-xcframework-local.sh macOSOnly StaticOnly
 
 xcodebuild -create-xcframework -framework Carthage/archive/Sentry/macosx.xcarchive/Products/Library/Frameworks/Sentry.framework -output Carthage/Sentry.xcframework
 
@@ -32,7 +32,7 @@ popd
 mkdir "${sentryArtifactsDestination}/bin"
 mkdir "${sentryArtifactsDestination}/include"
 
-cp "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry" "${sentryArtifactsDestination}/bin/libsentry.a"
+cp "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Sentry" "${sentryArtifactsDestination}/lib/libsentry.a"
 
 cp -rL "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/Headers" "${sentryArtifactsDestination}/include/Sentry"
 cp -rL "${sentryCocoaRoot}/Carthage/Sentry.xcframework/macos-arm64_arm64e_x86_64/Sentry.framework/PrivateHeaders/." "${sentryArtifactsDestination}/include/Sentry"
