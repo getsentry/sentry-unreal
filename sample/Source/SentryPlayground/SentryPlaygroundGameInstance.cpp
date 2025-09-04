1b02f8436f8a00fcd2f0ef187c8c6b89d093627f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-09-04 18:40:30 +0300

    chore: Update sample project to enable running integration tests in CI (#1064)
    * Update sample project to run integration tests
    
    * Cleanup test run in ci and upload sample build
    
    * Add sample project readme
    
    * Move assets
    
    * Fix contriburing guide
    
    * Fix path
    
    * Fix file tree
    
    * Update readme
    
    * Add dsn arg for integration tests sample
    
    * Sample build upload for UE 4.27
    
    * Rename arg
    
    * FIx multiple exit requests
    
    * Fix test reports path
    
    * Rename command line args to align with consoles ci naming
    
    * Set fake event id for test crash event
    
    * Update SentryPlaygroundGameInstance.cpp
    
    * Remove integration-test param
    
    * Try windows-2022
    
    * Fix string literals
    
    * Remove unreachable code block

--- sample/Source/SentryPlayground/SentryPlaygroundGameInstance.cpp
+++ sample/Source/SentryPlayground/SentryPlaygroundGameInstance.cpp
@@ -2,9 +2,122 @@
 
 #include "SentryPlaygroundGameInstance.h"
 
+#include "SentryPlayground.h"
 #include "SentrySubsystem.h"
+#include "SentrySettings.h"
+#include "SentryPlaygroundUtils.h"
+#include "SentryUser.h"
+
+#include "Misc/CommandLine.h"
+#include "Engine/Engine.h"
 
 void USentryPlaygroundGameInstance::Init()
 {
 	Super::Init();
+
+	const TCHAR* CommandLine = FCommandLine::Get();
+
+	// Check for expected test parameters to decide between running integration tests
+	// or launching the sample app with UI for manual testing
+	if (FParse::Param(FCommandLine::Get(), TEXT("crash-capture")) || 
+		FParse::Param(FCommandLine::Get(), TEXT("message-capture")))
+	{
+		RunIntegrationTest(CommandLine);
+	}
+}
+
+void USentryPlaygroundGameInstance::RunIntegrationTest(const TCHAR* CommandLine)
+{
+	UE_LOG(LogSentrySample, Log, TEXT("Running integration test for command: %s\n"), CommandLine);
+
+	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
+	if (!SentrySubsystem)
+	{
+		CompleteTestWithResult(TEXT("sentry-error"), false, TEXT("Invalid Sentry subsystem"));
+		return;
+	}
+
+	SentrySubsystem->InitializeWithSettings(FConfigureSettingsNativeDelegate::CreateLambda([=](USentrySettings* Settings)
+	{
+		// Override options set in config file if needed
+		FString Dsn;
+		if (FParse::Value(CommandLine, TEXT("dsn="), Dsn))
+		{
+			Settings->Dsn = Dsn;
+		}
+	}));
+
+	if (!SentrySubsystem->IsEnabled())
+	{
+		CompleteTestWithResult(TEXT("sentry-error"), false, TEXT("Failed to initialize Sentry"));
+		return;
+	}
+
+	SentrySubsystem->AddBreadcrumbWithParams(
+		TEXT("Integration test started"), TEXT("Test"), TEXT("info"), TMap<FString, FSentryVariant>(), ESentryLevel::Info);
+
+	ConfigureTestContext();
+
+	SentrySubsystem->AddBreadcrumbWithParams(
+		TEXT("Context configuration finished"), TEXT("Test"), TEXT("info"), TMap<FString, FSentryVariant>(), ESentryLevel::Info);
+
+	if (FParse::Param(CommandLine, TEXT("crash-capture")))
+	{
+		RunCrashTest();
+	}
+	else if (FParse::Param(CommandLine, TEXT("message-capture")))
+	{
+		RunMessageTest();
+	}
+}
+
+void USentryPlaygroundGameInstance::RunCrashTest()
+{
+	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
+
+	// Because we don't get the real crash event ID, create a fake one and set it as a tag
+	// This tag is then used by integration test script in CI to fetch the event
+
+	FString EventId = FGuid::NewGuid().ToString(EGuidFormats::Digits);
+
+	UE_LOG(LogSentrySample, Log, TEXT("EVENT_CAPTURED: %s\n"), *EventId);
+
+	SentrySubsystem->SetTag(TEXT("test.crash_id"), EventId);
+
+	USentryPlaygroundUtils::Terminate(ESentryAppTerminationType::NullPointer);
+}
+
+void USentryPlaygroundGameInstance::RunMessageTest()
+{
+	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
+
+	FString EventId = SentrySubsystem->CaptureMessage(TEXT("Integration test message"));
+
+	UE_LOG(LogSentrySample, Log, TEXT("EVENT_CAPTURED: %s\n"), *EventId);
+
+	CompleteTestWithResult(TEXT("message-capture"), !EventId.IsEmpty(), TEXT("Test complete"));
+}
+
+void USentryPlaygroundGameInstance::ConfigureTestContext()
+{
+	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
+
+	USentryUser* User = NewObject<USentryUser>();
+	User->Initialize();
+	User->SetUsername(TEXT("TestUser"));
+	User->SetEmail(TEXT("user-mail@test.abc"));
+	User->SetId(TEXT("12345"));
+
+	SentrySubsystem->SetUser(User);
+
+	SentrySubsystem->SetTag(TEXT("test.suite"), TEXT("integration"));
+}
+
+void USentryPlaygroundGameInstance::CompleteTestWithResult(const FString& TestName, bool Result, const FString& Message)
+{
+	UE_LOG(LogSentrySample, Log, TEXT("TEST_RESULT: {\"test\":\"%s\",\"success\":%s,\"message\":\"%s\"}\n"),
+		*TestName, Result ? TEXT("true") : TEXT("false"), *Message);
+
+	// Close app after test is completed
+	FGenericPlatformMisc::RequestExit(false);
 }
