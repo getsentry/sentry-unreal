47c5b30a10a2ab99b76c2c8f56461e05d7e3c433
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-09-10 19:04:43 +0300

    Update plugin-dev/Source/Sentry/Public/SentrySpan.h
    Co-authored-by: J-P Nurmi <jpnurmi@gmail.com>

--- plugin-dev/Source/Sentry/Public/SentrySpan.h
+++ plugin-dev/Source/Sentry/Public/SentrySpan.h
@@ -34,7 +34,7 @@ public:
 
 	/** Starts a new child span with timestamp.
 	 *
-	 * @note: On Android, if the span is bound the scope
+	 * @note: On Android, if the span is bound to scope
 	 * the SDK will put the new child span on the scope as well.
 	 */
 	UFUNCTION(BlueprintCallable, Category = "Sentry")
