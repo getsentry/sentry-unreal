78d91856814a7e9e6b93cf37521e7fe942c30121
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-01-15 16:07:06 +0100

    Fix

--- plugin-dev/Source/Sentry/Private/GenericPlatform/GenericPlatformSentryLog.cpp
+++ plugin-dev/Source/Sentry/Private/GenericPlatform/GenericPlatformSentryLog.cpp
@@ -37,8 +37,7 @@ void FGenericPlatformSentryLog::SetBody(const FString& body)
 FString FGenericPlatformSentryLog::GetBody() const
 {
 	sentry_value_t body = sentry_value_get_by_key(Log, "body");
-	const char* bodyStr = sentry_value_as_string(body);
-	return FString(bodyStr ? bodyStr : "");
+	return FString(UTF8_TO_TCHAR(sentry_value_as_string(body)));
 }
 
 void FGenericPlatformSentryLog::SetLevel(ESentryLevel level)
