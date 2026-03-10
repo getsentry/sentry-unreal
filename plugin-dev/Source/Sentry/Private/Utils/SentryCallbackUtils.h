0eb77d197245cd8b2a4e6583e9ee4ef75379078f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-03-10 17:37:52 +0200

    Add guard to prevent infinite recursion in callback handlers

--- plugin-dev/Source/Sentry/Private/Utils/SentryCallbackUtils.h
+++ plugin-dev/Source/Sentry/Private/Utils/SentryCallbackUtils.h
@@ -11,3 +11,37 @@ public:
 	 */
 	static bool IsCallbackSafeToRun();
 };
+
+/**
+ * RAII guard that prevents reentrant invocation of a specific callback type.
+ * Uses thread_local storage so each thread tracks reentrancy independently.
+ *
+ * Template parameter is used solely as a type tag to distinguish callback types -
+ * no instances of the tag type are created. Use the handler UClass types as tags
+ * (e.g. TSentryCallbackGuard<USentryBeforeSendHandler>).
+ */
+template<typename CallbackTag>
+class TSentryCallbackGuard
+{
+public:
+	TSentryCallbackGuard()
+		: bWasAlreadyActive(bIsActive)
+	{
+		bIsActive = true;
+	}
+
+	~TSentryCallbackGuard()
+	{
+		bIsActive = bWasAlreadyActive;
+	}
+
+	/** Returns true if this callback type was already active (reentrant call). */
+	bool IsReentrant() const { return bWasAlreadyActive; }
+
+private:
+	static thread_local bool bIsActive;
+	bool bWasAlreadyActive;
+};
+
+template<typename CallbackTag>
+thread_local bool TSentryCallbackGuard<CallbackTag>::bIsActive = false;
