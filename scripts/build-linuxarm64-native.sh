2f6909ebe98b6ce0407e5a02857e5bd5de0de06b
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-03-20 15:22:37 +0200

    Fix missing transport for native backend on Linux

--- scripts/build-linuxarm64-native.sh
+++ scripts/build-linuxarm64-native.sh
@@ -14,7 +14,8 @@ cmake --build "${sentryNativeRoot}/build_native" --target sentry --parallel
 cmake --install "${sentryNativeRoot}/build_native" --prefix "${sentryNativeRoot}/install_native"
 
 # Build sentry-native using clang and libstdc++ for sentry-crash executable
-cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build_native_crash" -D SENTRY_BACKEND=native -D SENTRY_TRANSPORT=none -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
+# Unlike crashpad_handler, sentry-crash needs transport (curl) to send crash envelopes
+cmake -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build_native_crash" -D SENTRY_BACKEND=native -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_C_COMPILER=clang-13 -D CMAKE_CXX_COMPILER="clang++-13" \
     -D CMAKE_CXX_FLAGS="-stdlib=libstdc++" -D CMAKE_EXE_LINKER_FLAGS="-stdlib=libstdc++"
 cmake --build "${sentryNativeRoot}/build_native_crash" --target sentry-crash --parallel
