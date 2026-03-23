01b7d5fcb09768772af0d1b649e9b3ef1640b7d8
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-03-23 14:13:01 +0200

    Clean up

--- sample/Source/SentryPlayground/SentryPlaygroundGameInstance.cpp
+++ sample/Source/SentryPlayground/SentryPlaygroundGameInstance.cpp
@@ -18,6 +18,7 @@
 #include "SentryVariant.h"
 
 #include "CoreGlobals.h"
+#include "SentryModule.h"
 #include "HAL/Platform.h"
 #include "Misc/CoreDelegates.h"
 #include "Misc/EngineVersionComparison.h"
@@ -128,6 +129,8 @@ void USentryPlaygroundGameInstance::RunIntegrationTest(const FString& CommandLin
 
 void USentryPlaygroundGameInstance::RunCrashTest(ESentryAppTerminationType CrashType)
 {
+	const USentrySettings* Settings = FSentryModule::Get().GetSettings();
+
 	USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
 
 	// Because we don't get the real crash event ID, create a fake one and set it as a tag
@@ -137,7 +140,16 @@ void USentryPlaygroundGameInstance::RunCrashTest(ESentryAppTerminationType Crash
 
 	// Workaround for duplicated log messages in UE 4.27 on Linux
 #if PLATFORM_LINUX && UE_VERSION_OLDER_THAN(5, 0, 0)
-	FPlatformMisc::LowLevelOutputDebugStringf(TEXT("EVENT_CAPTURED: %s\n"), *EventId);
+	if (Settings->UseNativeBackend)
+	{
+		// When using native backend, crashing process can terminate before engine’s GLog is flushed
+		// To ensure the event ID is not lost it is printed directly to stdout so the test script can reliably capture it
+		FPlatformMisc::LowLevelOutputDebugStringf(TEXT("EVENT_CAPTURED: %s\n"), *EventId);
+	}
+	else
+	{
+		UE_LOG(LogSentrySample, Log, TEXT("EVENT_CAPTURED: %s\n"), *EventId);
+	}
 #else
 	UE_LOG(LogSentrySample, Display, TEXT("EVENT_CAPTURED: %s\n"), *EventId);
 #endif
