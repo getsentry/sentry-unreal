9ff6017ee83bc664ac85933e914144c209bfa39d
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-03-23 20:08:37 +0200

    Fix compilation errors on older engine versions

--- plugin-dev/Source/Sentry/Private/Utils/SentryGCListener.cpp
+++ plugin-dev/Source/Sentry/Private/Utils/SentryGCListener.cpp
@@ -9,13 +9,21 @@
 
 #include "Engine/Engine.h"
 #include "HAL/PlatformTime.h"
+#include "Misc/EngineVersionComparison.h"
 #include "UObject/UObjectGlobals.h"
 
 FSentryGCListener::FSentryGCListener(TSharedPtr<FSentryPerformanceMetricAttributes> InMetricAttributes)
 	: GCStartTime(0.0)
 	, MetricAttributes(InMetricAttributes)
 {
+	// GetGarbageCollectStartedDelegate (5.5+) fires after the GC lock is acquired, measuring only the actual
+	// GC pause. GetPreGarbageCollectDelegate fires before lock acquisition, so on older engine versions the
+	// measurement may include time spent waiting for async threads to release the GC lock.
+#if UE_VERSION_OLDER_THAN(5, 5, 0)
+	GCStartedHandle = FCoreUObjectDelegates::GetPreGarbageCollectDelegate().AddRaw(this, &FSentryGCListener::OnGCStarted);
+#else
 	GCStartedHandle = FCoreUObjectDelegates::GetGarbageCollectStartedDelegate().AddRaw(this, &FSentryGCListener::OnGCStarted);
+#endif
 	PostGCHandle = FCoreUObjectDelegates::GetPostGarbageCollect().AddRaw(this, &FSentryGCListener::OnPostGC);
 
 	UE_LOG(LogSentrySdk, Log, TEXT("GC listener started."));
@@ -25,7 +33,11 @@ FSentryGCListener::~FSentryGCListener()
 {
 	if (GCStartedHandle.IsValid())
 	{
+#if UE_VERSION_OLDER_THAN(5, 5, 0)
+		FCoreUObjectDelegates::GetPreGarbageCollectDelegate().Remove(GCStartedHandle);
+#else
 		FCoreUObjectDelegates::GetGarbageCollectStartedDelegate().Remove(GCStartedHandle);
+#endif
 		GCStartedHandle.Reset();
 	}
 
