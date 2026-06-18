94b715965e4820e09ac55729f09e5d3f3390f22f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-06-18 11:28:16 +0300

    Add replay info struct

---
+++ plugin-dev/Source/Sentry/Private/SessionReplay/SentryReplayInfo.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 Sentry. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * Description of a recorded session-replay clip.
+ * 
+ * See https://develop.sentry.dev/sdk/telemetry/replays/ for details.
+ */
+struct FSentryReplayInfo
+{
+	FString ReplayId;
+
+	// Segment index within the replay (0 for the single crash-time buffer segment)
+	int32 SegmentId = 0;
+
+	// Replay capture mode: "buffer" (rolling window) or "session" (continuous)
+	FString ReplayType = TEXT("buffer");
+
+	double StartTimestampSec = 0.0;
+	double EndTimestampSec = 0.0;
+
+	FString ErrorEventId;
+
+	FString VideoPath;
+
+	int32 Width = 0;
+	int32 Height = 0;
+
+	int64 DurationMs = 0;
+	int64 SizeBytes = 0;
+	int32 FrameCount = 0;
+	int32 FrameRate = 0;
+};
