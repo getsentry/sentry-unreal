0ea6ddfae7f7ac26f5bd3851e2c8a6b016d60f4f
Ivan Tustanivskyi <tustanivsky@gmail.com>
2026-07-28 10:00:38 +0300

    Add note explaining SENTRY_HANDLER_STACK_SIZE purpose in build scripts

--- scripts/build-win64-crashpad.sh
+++ scripts/build-win64-crashpad.sh
@@ -7,6 +7,7 @@ export sentryArtifactsDestination=$2
 rm -rf "${sentryArtifactsDestination}/"*
 
 # Force CMake to use the VS2019 toolset (`-T v142`) to maintain native libraries compatibility with older Unreal Engine versions (4.27–5.1)
+# Set `SENTRY_HANDLER_STACK_SIZE` to improve stack-overflow crash capture reliability
 cmake -G "Visual Studio 17 2022" -T v142 -S "${sentryNativeRoot}" -B "${sentryNativeRoot}/build" -D SENTRY_BACKEND=crashpad -D SENTRY_SDK_NAME=sentry.native.unreal -D SENTRY_BUILD_SHARED_LIBS=OFF -D SENTRY_BATCHER_BUFFER_COUNT=10 -D SENTRY_HANDLER_STACK_SIZE=128
 cmake --build "${sentryNativeRoot}/build" --target sentry --config RelWithDebInfo --parallel
 cmake --build "${sentryNativeRoot}/build" --target crashpad_handler --config RelWithDebInfo --parallel
